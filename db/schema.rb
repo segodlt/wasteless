3850138a21df744e79e2b2c811ba4c0865c99728
Segolene de la Taille <segolette@hotmail.com>
2020-08-31 14:58:58 +0200

    suppr colonne listed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_173604) do
+ActiveRecord::Schema.define(version: 2020_08_31_125651) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,7 +76,6 @@ ActiveRecord::Schema.define(version: 2020_08_29_173604) do
     t.bigint "recipe_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "listed", default: false
     t.index ["ingredient_id"], name: "index_measures_on_ingredient_id"
     t.index ["recipe_id"], name: "index_measures_on_recipe_id"
   end
